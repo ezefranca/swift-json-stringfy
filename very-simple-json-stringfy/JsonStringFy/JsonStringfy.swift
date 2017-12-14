f5eb6ebe779a7b6b8a06a2a97fb51131478dd52f
Gabriel Tomaz <Gabriel.Tomaz@btgpactual.com>
2017-12-14 16:21:39 -0200

    improvements and split metod to print and to string

--- very-simple-json-stringfy/JsonStringFy/JsonStringfy.swift
+++ very-simple-json-stringfy/JsonStringFy/JsonStringfy.swift
@@ -8,17 +8,32 @@ import Foundation
 
 public class JsonStringfy {
     
-    /// Pretty Print JSON FIles in Human readable Strings
+    
+    /// Pretty String JSON FIles in Human readable Strings
     ///
     /// - Parameter json: json to be printed
     /// - Returns: stringfyed json
-    static func prettyPrint(with json: [String:Any]) -> String {
+    static func prettyString(_ json: [String:Any]) -> String {
         do {
             let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-            let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-            return string! as String
+            let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String?
+            return string ?? "Invalid or empty json"
         } catch {
             return "Invalid or empty json"
+        }
+    }
+    
+    
+    /// Pretty Print JSON FIles in Human readable Strings
+    ///
+    /// - Parameter json: json to be printed
+    static func prettyPrint(_ json: [String:Any]) {
+        do {
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String?
+            print(string ?? "Invalid or empty json")
+        } catch {
+            print("Invalid or empty json")
         }
     }
     
